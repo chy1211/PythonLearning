6c35cc8bcaddd57d2526ae79d294b0222ea34c52
chy1211 <henry70304@gmail.com>
2021-04-21 14:42:20 +0800

    add pwd.py

---
+++ c109193244/pwd.py
@@ -0,0 +1,18 @@
+import os
+print(os.getcwd())
+
+fp=open("record.txt","r")
+
+tmp=fp.readline()
+
+tmp1=tmp.replace("\n","")
+tmp2=tmp1.split(",")
+print(tmp2)
+
+while tmp != "":
+    tmp=fp.readline()
+    tmp1=tmp.replace("\n","")
+    tmp2=tmp1.split(",")
+    print(tmp2)
+
+fp.close()
