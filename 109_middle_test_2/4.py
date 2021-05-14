4ca4403bc5a2192ca90a960e0f13a90bf7a26271
chy1211 <henry70304@gmail.com>
2021-05-14 13:17:08 +0800

    new middle test

---
+++ 109_middle_test_2/4.py
@@ -0,0 +1,20 @@
+class University:
+    def __init__(self,name):
+        self.name=name
+        self.cps=[]
+class Campus:
+    def __init__(self,name):
+        self.name=name
+        self.dps=[]
+class Departmant:
+    def __init__(self,name):
+        self.name=name
+cpsname=["建工校區","旗津校區","第一校區","楠梓校區","燕巢校區"]
+dpsname=["智商","金資","會資","財稅","觀光"]
+nkust=University("高雄科技大學")
+for i in cpsname:
+    print(i)
+    nkust.cps.append(Campus(i))
+for i in dpsname:
+    print(i)
+    nkust.cps[4].dps.append(Departmant(i))
