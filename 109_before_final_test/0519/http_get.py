d52efbc6d7e7dc2ca466ee357a117a20791baacf
chy1211 <henry70304@gmail.com>
2021-05-25 13:36:10 +0800

    New Daily Practice

---
+++ 109_before_final_test/0519/http_get.py
@@ -0,0 +1,10 @@
+import socket
+
+host = "192.168.56.67"
+port = 80
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
+s.sendall(b"GET /~handsome/testget.php?username=Testusername HTTP/1.0\r\n\r\n")
+data = s.recv(1024)
+s.close()
+print("Received", data.decode("utf-8"))
